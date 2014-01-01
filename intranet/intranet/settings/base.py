1ded1ebca174e812176e470752b1dc04654a2c9d
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2014-01-01 21:08:34 +0100

    Añadiendo varios settings uno para los ajustes locales y otros para los ajustes locales

--- intranet/intranet/settings/base.py
+++ intranet/intranet/settings/base.py
@@ -1,33 +1,22 @@
-"""
-Django settings for intranet project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
+# -*- coding: utf-8 -*-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+from unipath import Path
+PROJECT_DIR = Path(__file__).ancestor(3)
+MEDIA_ROOT = PROJECT_DIR.child("media")
+STATIC_ROOT = PROJECT_DIR.child("static")
+STATICFILES_DIRS = (
+    PROJECT_DIR.child("assets"),
+)
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child("templates"),
+)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY
 SECRET_KEY = '(__b8z0w!g@2vgn&ojyk0@*gpq_01!^x=br1mb*&^xqhoy*@w%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -48,35 +37,15 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'intranet.urls'
-
 WSGI_APPLICATION = 'intranet.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es-es'
+TIME_ZONE = 'Europe/Madrid'
+SITE_ID = 1
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
---
+++ intranet/intranet/settings/local.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# settings/local.py
+from .base import *
+
+# SECURITY
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+# EMAIL SERVER
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
+
+# DATABASE LOCAL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
